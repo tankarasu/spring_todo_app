5878704700eb2c8f0ca0dbecd0e6c7391601220d
TanKARASU <karasutan@gmail.com>
2022-04-02 21:52:06 +0200

    Docs: documentation added, uml Also

--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,100 @@
 
 this is a simple todo app api handled with Java Spring Boot. This repo 
 handles for now only the back-end. 
+
+## Running Test
+
+## API endpoints
+
+### GET
+Fetch all task.
+``` 
+http://localhost:8000/tasks  
+
+Method : GET  
+ResponseBody : List-Task
+```
+
+Fetch a specific task.
+``` 
+http://localhost:8000/tasks/id  
+
+Method : GET  
+ResponseBody : Task
+```
+
+### POST
+
+Create a new task.
+``` 
+http://localhost:8000/tasks 
+
+Method : POST
+RequestBody : Task  
+ResponseBody : Task
+```
+
+### PUT
+
+Modify an existing task.
+``` 
+http://localhost:8000/tasks 
+
+Method : PUT
+RequestBody : Task  
+ResponseBody : Task
+```
+
+Modify an existing task's finished state.
+``` 
+http://localhost:8000/tasks/finished
+
+Method : PUT
+RequestParam : id  
+```
+
+### DELETE
+
+Delete an existing task.
+``` 
+http://localhost:8000/tasks 
+
+Method : PUT
+RequestParam : id  
+```
+
+---
+
+## Further Help
+
+Developer can find more help by reading the test case in UT (unit testing) 
+and FT (functional testing) folders.
+
+### TaskService
+
+Globally, a developer will never use directly the Service. This part is only 
+for information purpose. 
+
+### TaskService.setTasks( )
+
+This method is used for Testing purpose.
+
+### TaskService.fetchAllTasks( )
+
+This method is used to fetch all Tasks inside a List of Tasks.
+
+### TaskService.fetchSpecificTask( )
+
+This method is used to fetch a specific Task.
+
+### TaskService.createOneTask( )
+
+This method is used when we want to create a new Task.
+
+### TaskService.updateSpecificTask( )
+
+This method is used when we want to update a specific Task.
+
+### TaskService.deleteSpecificTask( )
+
+This method is used when we want to delete a specific Task. 
--- src/main/java/com/tankarau/spring_todo_app/services/TaskService.java
+++ src/main/java/com/tankarau/spring_todo_app/services/TaskService.java
@@ -1,8 +1,12 @@
 package com.tankarau.spring_todo_app.services;
 
+// Internal requirements
 import com.tankarau.spring_todo_app.models.Task;
+
+// Third-party requirements
 import org.springframework.stereotype.Service;
 
+// Core-module requirements
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +14,10 @@ import java.util.Objects;
 
 @Service
 public class TaskService {
+    /**
+     * For Testing Purpose
+     * @param tasks initial taskList for the parametrized testing
+     */
     public void setTasks(List<Task> tasks) {
         this.tasks = tasks;
     }
@@ -20,22 +28,42 @@ public class TaskService {
             new Task("3", "task 3")
     ));
 
+    /**
+     * used to fetch all available tasks
+     * @return a list of ask List
+     */
     public List<Task> fetchAllTasks() {
         return tasks;
     }
 
+    /**
+     * used to fetch a specific tasks
+     * @param id id of specific task
+     * @return a specific Task
+     */
     public Task fetchSpecificTask(String id) {
         return tasks.stream().filter(task -> Objects.equals(task.getId(), id))
                 .findFirst()
-                .get();
+                .orElse(null);
     }
 
+    /**
+     * Used for task creation
+     * @param newTask task to be created
+     * @return return the newly created task
+     */
     public Task createOneTask(Task newTask) {
         tasks.add(newTask);
 
         return newTask;
     }
 
+    /**
+     * used for updating a Task
+     * @param id specific task ID
+     * @param updatedTask updated Task
+     * @return the updated Task if success
+     */
     public Task updateSpecificTask(String id, Task updatedTask) {
         for (Task task : tasks)
             if (task.getId().equals(id))
@@ -44,6 +72,11 @@ public class TaskService {
         return fetchSpecificTask(id);
     }
 
+    /**
+     * used to delete a specific Task
+     * @param id the specific Task to delete
+     * @return True if success otherwise false
+     */
     public Boolean deleteSpecificTask(String id) {
         return tasks.removeIf(task -> task.getId().equals(id));
     }
